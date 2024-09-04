ef39860e98038b47623834d0b8f2a937359867eb
ivanurbanodev <ivan.urbano.n@uni.pe>
2024-09-03 22:07:34 -0500

    corregido

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@
  });
  const port = process.env.PORT || 3000;
  app.listen(port, () => {
- console.log(Server running on port ${port});
+ console.log(`Server running on port ${port}`);
  });
  module.exports = app;
 
